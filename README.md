e483c847a19bba7bc80e333045f8b0c65f6991df
marc <mlasch@mailbox.org>
2019-05-20 12:28:46 +0200

    Updated Docker environment to run the mqtt broker locally.

--- README.md
+++ README.md
@@ -13,19 +13,34 @@ $ sudo apt install python3-venv python3-dev libsdl-dev libsdl-image1.2-dev libsd
 Create a virtual environment for `python3`
 
 ```
-$ python3 -m venv python-venv
+$ python3 -m venv .venv
 ```
 
 Install requirements from the file
 
 ```
-$ source python-venv/bin/activate
-(python-venv)$ pip3 install -r requirements.txt
+$ source .venv/bin/activate
+(.venv)$ pip3 install -r requirements.txt
 ```
 
 ### Message Broker
 
+Install the message broker locally:
+
 ```
 $ sudo apt install mosquitto mosquitto-clients
 ```
+or run it inside a Docker container:
 
+```
+$ cd docker
+$ docker build -t mqtt-broker . 
+```
+
+```
+$ docker run -p 1883:1883 -t mqtt-broker
+```
+
+List the installed Docker images with `docker image ls` or `docker system df` in either case sometimes it's necessary to reset the environment: `docker system prune`  
+
+Tagging the Docker image after building: `docker tag 3b6b527f221b mqtt-broker` 
--- pos_emitter.py
+++ pos_emitter.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import math
+import argparse
+
 from random import randint
 from time import sleep
 from struct import pack
@@ -9,13 +12,25 @@ import paho.mqtt.client as mqtt
 from planning.position import Emitter
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Robot position simulator')
+    parser.add_argument('hostname', metavar='HOST', help='Hostname of the MQTT broker')
+    args = parser.parse_args()
+
+
     client = mqtt.Client('emitter-'+''.join(str(randint(0,9)) for _ in range(5)))
-    client.connect('192.168.6.235', port=1883, keepalive=60, bind_address="")
+    #client.connect('localhost', port=1883, keepalive=60, bind_address="")
+    client.connect(args.hostname, port=1883, keepalive=60, bind_address="")
 
     emitter = Emitter(1700, 2800, 0)
 
     for x, y, theta in emitter.emit():
-        payload = pack("!fff", x, y, theta)
+        x = 4000.0
+        y = 2000.0
+        ctl_forward = 0
+        ctl_side = 0
+        theta = -math.pi/2
+
+        payload = pack("!fffff", x, y, theta, ctl_forward, ctl_side)
         client.publish("position", payload=payload, qos=0, retain=False)
         print("send {} {} {} {}".format(x,y,theta, payload))
-        sleep(0.08)
+        sleep(0.5)
